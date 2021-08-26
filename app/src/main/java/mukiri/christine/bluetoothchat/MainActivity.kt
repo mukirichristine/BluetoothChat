bb78f758be23937d06f5e7fa50548009c95e1c10
ChristineMukiri <54032340+ChristineMukiri@users.noreply.github.com>
2021-08-26 18:30:19 +0300

    Dynamically add items to RecyclerView

--- app/src/main/java/mukiri/christine/bluetoothchat/MainActivity.kt
+++ app/src/main/java/mukiri/christine/bluetoothchat/MainActivity.kt
@@ -1,19 +1,32 @@
 package mukiri.christine.bluetoothchat
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import mukiri.christine.bluetoothchat.adapters.TextAdapter
+import mukiri.christine.bluetoothchat.databinding.ActivityMainBinding
 import mukiri.christine.bluetoothchat.models.TextMessage
+import org.w3c.dom.Text
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
-        recyclerview.layoutManager = LinearLayoutManager(this)
+        val timeFormatter = DateTimeFormatter.ofPattern("hh:mm a")
+
+        binding.recyclerview.layoutManager = LinearLayoutManager(this)
 
         val data = ArrayList<TextMessage>()
         data.add(TextMessage("7:53pm", "Hi"))
@@ -22,7 +35,22 @@ class MainActivity : AppCompatActivity() {
 
         val adapter = TextAdapter(data)
 
-        recyclerview.adapter = adapter
+        binding.recyclerview.adapter = adapter
+
+        binding.btnSend.setOnClickListener{
+            val dateTime = LocalDateTime.now()
+
+            data.add(
+                TextMessage(
+                    timeFormatter.format(dateTime),
+                    binding.newMessage.getText().toString()
+                )
+            )
+
+            adapter.notifyDataSetChanged()
+            binding.recyclerview.smoothScrollToPosition(adapter.getItemCount() - 1)
+            binding.newMessage.setText("")
+        }
 
 
     }
